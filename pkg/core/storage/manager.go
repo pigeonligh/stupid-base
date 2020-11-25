ac41892f3deffe87377ed089b1ff4e6732f61cc0
pigeonligh <pigeonligh@hotmail.com>
2020-11-26 01:34:25 +0800

    feat: add simple log

--- pkg/core/storage/manager.go
+++ pkg/core/storage/manager.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/pigeonligh/stupid-base/pkg/core/storage/buffer"
 	"github.com/pigeonligh/stupid-base/pkg/core/types"
+	"github.com/pigeonligh/stupid-base/pkg/log"
 )
 
 const bufferSize = 64
@@ -28,6 +29,8 @@ func GetInstance() *Manager {
 }
 
 func init() {
+	log.V(2).Info("Storage Manager starts to initialize.")
+	defer log.V(2).Info("Storage Manager has been initialized.")
 	instance = &Manager{
 		buffer: buffer.NewManager(bufferSize, types.PageSize),
 	}

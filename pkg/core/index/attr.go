9604d1adc45289b6f66b468e3143489b91dd44e0
pigeonligh <pigeonligh@hotmail.com>
2020-12-21 15:57:49 +0800

    null attr allowed

--- pkg/core/index/attr.go
+++ pkg/core/index/attr.go
@@ -7,20 +7,32 @@ type SingleAttr struct {
 }
 
 type AttrDefine struct {
-	attrs []SingleAttr
+	attrs   []SingleAttr
+	nullPos []int
 }
 
 func NewAttr() *AttrDefine {
-	return &AttrDefine{}
+	return &AttrDefine{
+		attrs:   []SingleAttr{},
+		nullPos: []int{},
+	}
 }
 
 func loadAttrFromHeader(header *types.IndexHeaderPage) *AttrDefine {
 	attrs := []SingleAttr{}
+	nullPos := []int{}
+	pos := 0
 	for i := 0; i < header.AttrSize; i++ {
 		attrs = append(attrs, SingleAttr{AttrInfo: header.Attrs[i]})
+		pos += header.Attrs[i].AttrSize
+		if header.Attrs[i].NullAllowed {
+			nullPos = append(nullPos, pos)
+			pos++
+		}
 	}
 	return &AttrDefine{
-		attrs: attrs,
+		attrs:   attrs,
+		nullPos: nullPos,
 	}
 }
 
@@ -36,13 +48,25 @@ func (attr *AttrDefine) dataToAttrs(rid types.RID, data []byte) []byte {
 	size := len(attr.attrs)
 	for i := 0; i < size; i++ {
 		sa := attr.attrs[i]
-		tmpSlice := data[sa.AttrOffset : sa.AttrOffset+sa.AttrSize]
+		attrsize := sa.AttrSize
+		if sa.NullAllowed {
+			attrsize++
+		}
+		tmpSlice := data[sa.AttrOffset : sa.AttrOffset+attrsize]
 		result = append(result, tmpSlice...)
-		// TODO: need some discuss
 	}
 	return result
 }
 
 func (attr *AttrDefine) AddSingleAttr(sa SingleAttr) {
 	attr.attrs = append(attr.attrs, sa)
+}
+
+func (attr *AttrDefine) HasNull(attrData []byte) bool {
+	for _, pos := range attr.nullPos {
+		if attrData[pos] > 0 {
+			return true
+		}
+	}
+	return false
 }
